836b8bdc2c1ad6224aa9dd439f3cf4ee72587510
Raina <singhamrit2007@gmail.com>
2026-07-23 17:50:33 -0400

    feature/sprint2: cleanup docker and fix lint-7

--- apps/api/internal/metrics/metrics.go
+++ apps/api/internal/metrics/metrics.go
@@ -1,10 +1,11 @@
 package metrics
 
 import (
-	appruntime "github.com/amyismebyme/the-village/apps/api/internal/runtime"
-	"github.com/prometheus/client_golang/prometheus"
 	"runtime"
 	"sync"
+
+	appruntime "github.com/amyismebyme/the-village/apps/api/internal/runtime"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var RequestsTotal = prometheus.NewCounterVec(
@@ -21,13 +22,72 @@ var RequestsTotal = prometheus.NewCounterVec(
 
 var RequestDuration = prometheus.NewHistogramVec(
 	prometheus.HistogramOpts{
-		Name:    "village_http_request_duration_seconds",
-		Help:    "HTTP request latency.",
-		Buckets: prometheus.DefBuckets,
+		Name: "village_http_request_duration_seconds",
+		Help: "HTTP request latency.",
+		Buckets: []float64{
+			0.001,
+			0.0025,
+			0.005,
+			0.01,
+			0.025,
+			0.05,
+			0.1,
+			0.25,
+			0.5,
+			1,
+			2.5,
+			5,
+			10,
+		},
 	},
 	[]string{
 		"method",
 		"path",
+	},
+)
+
+var RequestsInFlight = prometheus.NewGauge(
+	prometheus.GaugeOpts{
+		Name: "village_http_requests_in_flight",
+		Help: "Current number of HTTP requests being processed.",
+	},
+)
+
+var PanicsTotal = prometheus.NewCounter(
+	prometheus.CounterOpts{
+		Name: "village_panics_total",
+		Help: "Total recovered panics.",
+	},
+)
+
+var ErrorsTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "village_errors_total",
+		Help: "Total application errors.",
+	},
+	[]string{
+		"type",
+	},
+)
+
+var DatabaseQueriesTotal = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "village_db_queries_total",
+		Help: "Total database queries.",
+	},
+	[]string{
+		"operation",
+	},
+)
+
+var DatabaseQueryDuration = prometheus.NewHistogramVec(
+	prometheus.HistogramOpts{
+		Name:    "village_db_query_duration_seconds",
+		Help:    "Database query latency.",
+		Buckets: prometheus.DefBuckets,
+	},
+	[]string{
+		"operation",
 	},
 )
 
@@ -51,7 +111,16 @@ func Register() {
 		prometheus.MustRegister(
 			RequestsTotal,
 			RequestDuration,
+			RequestsInFlight,
+			PanicsTotal,
+			ErrorsTotal,
+			DatabaseQueriesTotal,
+			DatabaseQueryDuration,
 			BuildInfo,
+
+			// Standard Go & process metrics.
+			//prometheus.NewGoCollector(),
+			//prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
 		)
 
 		BuildInfo.WithLabelValues(
