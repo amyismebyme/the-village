4e9be26a938b3232b73fd43fe0bf907d7d45a0d6
Raina <singhamrit2007@gmail.com>
2026-07-28 20:03:03 -0400

    feature-milestone7-Sprint6-connectino pool tuning 2

--- apps/api/internal/app/dependencies.go
+++ apps/api/internal/app/dependencies.go
@@ -1,15 +1,13 @@
 package app
 
 import (
-	"log/slog"
 	"github.com/amyismebyme/the-village/apps/api/internal/database"
 	"github.com/amyismebyme/the-village/apps/api/internal/health"
+	"log/slog"
 )
 
 type Dependencies struct {
 	Logger *slog.Logger
-
-
 
 	DB     *database.Database
 	Health *health.Registry
